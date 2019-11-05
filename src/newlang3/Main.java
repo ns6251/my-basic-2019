217aca442c9016351a4942973fa86a9d6a5754b6
ns6251 <ns6251@gmail.com>
2019-11-05 15:02:07 +0900

    Initial commit

---
+++ src/newlang3/Main.java
@@ -0,0 +1,9 @@
+package newlang3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ src/newlang3/ValueType.java
@@ -0,0 +1,5 @@
+package newlang3;
+
+public enum ValueType {
+    VOID, INTEGER, STRING, DOUBLE, BOOL,
+}
