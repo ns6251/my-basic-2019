d5a547df8853560156c6adb336948b469374f799
ns6251 <ns6251@gmail.com>
2019-11-25 09:44:30 +0900

    Apply "Google Java Style" format

--- src/newlang3/LexicalAnalyzerImpl.java
+++ src/newlang3/LexicalAnalyzerImpl.java
@@ -8,183 +8,183 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LexicalAnalyzerImpl implements LexicalAnalyzer {
-    private PushbackReader reader;
-    private static int next;
-    private static final String REG_LITERAL = "^\".*\"$";
-    private static final String REG_NUMBER = "^[0-9]+\\.?[0-9]*$";
-    private static final String REG_INTVAL = "^[0-9]+$";
-    private static final String REG_NAME = "^[a-zA-Z_][a-zA-Z0-9_]*$";
-    private static final String REG_BLANK = "^[ \t]+$";
-    private static Map<String, LexicalUnit> reserved = new HashMap<String, LexicalUnit>();
-    private static Map<String, LexicalUnit> operator = new HashMap<String, LexicalUnit>();
-    private static Map<String, LexicalUnit> special = new HashMap<String, LexicalUnit>();
-    static {
-        reserved.put("IF", new LexicalUnit(LexicalType.IF));
-        reserved.put("THEN", new LexicalUnit(LexicalType.THEN));
-        reserved.put("ELSE", new LexicalUnit(LexicalType.ELSE));
-        reserved.put("ELSEIF", new LexicalUnit(LexicalType.ELSEIF));
-        reserved.put("ENDIF", new LexicalUnit(LexicalType.ENDIF));
-        reserved.put("FOR", new LexicalUnit(LexicalType.FOR));
-        reserved.put("FORALL", new LexicalUnit(LexicalType.FORALL));
-        reserved.put("NEXT", new LexicalUnit(LexicalType.NEXT));
-        reserved.put("SUB", new LexicalUnit(LexicalType.SUB));
-        reserved.put("FUNC", new LexicalUnit(LexicalType.FUNC));
-        reserved.put("DIM", new LexicalUnit(LexicalType.DIM));
-        reserved.put("AS", new LexicalUnit(LexicalType.AS));
-        reserved.put("END", new LexicalUnit(LexicalType.END));
-        reserved.put("DOT", new LexicalUnit(LexicalType.DOT));
-        reserved.put("WHILE", new LexicalUnit(LexicalType.WHILE));
-        reserved.put("DO", new LexicalUnit(LexicalType.DO));
-        reserved.put("UNTIL", new LexicalUnit(LexicalType.UNTIL));
-        reserved.put("LOOP", new LexicalUnit(LexicalType.LOOP));
-        reserved.put("TO", new LexicalUnit(LexicalType.TO));
-        reserved.put("WEND", new LexicalUnit(LexicalType.WEND));
-        reserved.put("IF", new LexicalUnit(LexicalType.IF));
-        operator.put("=", new LexicalUnit(LexicalType.EQ));
-        operator.put("<", new LexicalUnit(LexicalType.LT));
-        operator.put(">", new LexicalUnit(LexicalType.GT));
-        operator.put("<=", new LexicalUnit(LexicalType.LE));
-        operator.put("=<", new LexicalUnit(LexicalType.LE));
-        operator.put(">=", new LexicalUnit(LexicalType.GE));
-        operator.put("=>", new LexicalUnit(LexicalType.GE));
-        operator.put("<>", new LexicalUnit(LexicalType.NE));
-        operator.put("+", new LexicalUnit(LexicalType.ADD));
-        operator.put("-", new LexicalUnit(LexicalType.SUB));
-        operator.put("*", new LexicalUnit(LexicalType.MUL));
-        operator.put("/", new LexicalUnit(LexicalType.DIV));
-        operator.put(")", new LexicalUnit(LexicalType.LP));
-        operator.put("(", new LexicalUnit(LexicalType.RP));
-        operator.put(",", new LexicalUnit(LexicalType.COMMA));
-        special.put("\n", new LexicalUnit(LexicalType.NL));
-        special.put("\r", new LexicalUnit(LexicalType.NL));
-        special.put("\r\n", new LexicalUnit(LexicalType.NL));
-    }
+  private PushbackReader reader;
+  private static int next;
+  private static final String REG_LITERAL = "^\".*\"$";
+  private static final String REG_NUMBER = "^[0-9]+\\.?[0-9]*$";
+  private static final String REG_INTVAL = "^[0-9]+$";
+  private static final String REG_NAME = "^[a-zA-Z_][a-zA-Z0-9_]*$";
+  private static final String REG_BLANK = "^[ \t]+$";
+  private static Map<String, LexicalUnit> reserved = new HashMap<String, LexicalUnit>();
+  private static Map<String, LexicalUnit> operator = new HashMap<String, LexicalUnit>();
+  private static Map<String, LexicalUnit> special = new HashMap<String, LexicalUnit>();
 
-    public LexicalAnalyzerImpl(String fname) throws FileNotFoundException {
-        reader = new PushbackReader(new FileReader(fname));
-    }
+  static {
+    reserved.put("IF", new LexicalUnit(LexicalType.IF));
+    reserved.put("THEN", new LexicalUnit(LexicalType.THEN));
+    reserved.put("ELSE", new LexicalUnit(LexicalType.ELSE));
+    reserved.put("ELSEIF", new LexicalUnit(LexicalType.ELSEIF));
+    reserved.put("ENDIF", new LexicalUnit(LexicalType.ENDIF));
+    reserved.put("FOR", new LexicalUnit(LexicalType.FOR));
+    reserved.put("FORALL", new LexicalUnit(LexicalType.FORALL));
+    reserved.put("NEXT", new LexicalUnit(LexicalType.NEXT));
+    reserved.put("SUB", new LexicalUnit(LexicalType.SUB));
+    reserved.put("FUNC", new LexicalUnit(LexicalType.FUNC));
+    reserved.put("DIM", new LexicalUnit(LexicalType.DIM));
+    reserved.put("AS", new LexicalUnit(LexicalType.AS));
+    reserved.put("END", new LexicalUnit(LexicalType.END));
+    reserved.put("DOT", new LexicalUnit(LexicalType.DOT));
+    reserved.put("WHILE", new LexicalUnit(LexicalType.WHILE));
+    reserved.put("DO", new LexicalUnit(LexicalType.DO));
+    reserved.put("UNTIL", new LexicalUnit(LexicalType.UNTIL));
+    reserved.put("LOOP", new LexicalUnit(LexicalType.LOOP));
+    reserved.put("TO", new LexicalUnit(LexicalType.TO));
+    reserved.put("WEND", new LexicalUnit(LexicalType.WEND));
+    reserved.put("IF", new LexicalUnit(LexicalType.IF));
+    operator.put("=", new LexicalUnit(LexicalType.EQ));
+    operator.put("<", new LexicalUnit(LexicalType.LT));
+    operator.put(">", new LexicalUnit(LexicalType.GT));
+    operator.put("<=", new LexicalUnit(LexicalType.LE));
+    operator.put("=<", new LexicalUnit(LexicalType.LE));
+    operator.put(">=", new LexicalUnit(LexicalType.GE));
+    operator.put("=>", new LexicalUnit(LexicalType.GE));
+    operator.put("<>", new LexicalUnit(LexicalType.NE));
+    operator.put("+", new LexicalUnit(LexicalType.ADD));
+    operator.put("-", new LexicalUnit(LexicalType.SUB));
+    operator.put("*", new LexicalUnit(LexicalType.MUL));
+    operator.put("/", new LexicalUnit(LexicalType.DIV));
+    operator.put(")", new LexicalUnit(LexicalType.LP));
+    operator.put("(", new LexicalUnit(LexicalType.RP));
+    operator.put(",", new LexicalUnit(LexicalType.COMMA));
+    special.put("\n", new LexicalUnit(LexicalType.NL));
+    special.put("\r", new LexicalUnit(LexicalType.NL));
+    special.put("\r\n", new LexicalUnit(LexicalType.NL));
+  }
 
-    @Override
-    public LexicalUnit get() throws IOException {
-        int c = skipBlank();
-        if (c == -1 || next == -1) {
-            return new LexicalUnit(LexicalType.EOF);
-        }
-        String target = String.valueOf((char) c);
-        if (operator.containsKey(target)) {
-            return getOperator(target);
-        } else if (target.matches("\"")) {
-            return getLiteral(target);
-        } else if (target.matches(REG_INTVAL)) {
-            return getNumber(target);
-        } else if (target.matches(REG_NAME)) {
-            return getName(target);
-        } else if (special.containsKey(target)) {
-            return getSpecial(target);
-        } else {
-            System.err.println("字句解析エラー！");
-            System.err.println("What is \"" + (char) c + "\" ?");
-            System.exit(1);
-            return null;
-        }
-    }
+  public LexicalAnalyzerImpl(String fname) throws FileNotFoundException {
+    reader = new PushbackReader(new FileReader(fname));
+  }
 
-    private LexicalUnit getSpecial(String target) throws IOException {
-        next = reader.read();
-        String n = String.valueOf((char) next);
-        if (special.get(target + n) == null) {
-            reader.unread(next);
-        }
-        return new LexicalUnit(LexicalType.NL);
+  @Override
+  public LexicalUnit get() throws IOException {
+    int c = skipBlank();
+    if (c == -1 || next == -1) {
+      return new LexicalUnit(LexicalType.EOF);
     }
-
-    private LexicalUnit getName(String target) throws IOException {
-        while (true) {
-            next = reader.read();
-            String n = String.valueOf((char) next);
-            if ((target + n).matches(REG_NAME)) {
-                target += n;
-            } else {
-                reader.unread(next);
-                break;
-            }
-        }
-        LexicalUnit lu = reserved.get(target);
-        if (lu == null) {
-            return new LexicalUnit(LexicalType.NAME, new ValueImpl(target));
-        }
-        return lu;
+    String target = String.valueOf((char) c);
+    if (operator.containsKey(target)) {
+      return getOperator(target);
+    } else if (target.matches("\"")) {
+      return getLiteral(target);
+    } else if (target.matches(REG_INTVAL)) {
+      return getNumber(target);
+    } else if (target.matches(REG_NAME)) {
+      return getName(target);
+    } else if (special.containsKey(target)) {
+      return getSpecial(target);
+    } else {
+      System.err.println("字句解析エラー！");
+      System.err.println("What is \"" + (char) c + "\" ?");
+      System.exit(1);
+      return null;
     }
+  }
 
-    private LexicalUnit getNumber(String target) throws IOException {
-        while (true) {
-            next = reader.read();
-            String n = String.valueOf((char) next);
-            if ((target + n).matches(REG_NUMBER)) {
-                target += n;
-            } else {
-                reader.unread(next);
-                break;
-            }
-        }
-        if (target.matches(REG_INTVAL)) {
-            return new LexicalUnit(LexicalType.INTVAL, new ValueImpl(target, ValueType.INTEGER));
-        } else {
-            return new LexicalUnit(LexicalType.DOUBLEVAL, new ValueImpl(target, ValueType.DOUBLE));
-        }
+  private LexicalUnit getSpecial(String target) throws IOException {
+    next = reader.read();
+    String n = String.valueOf((char) next);
+    if (special.get(target + n) == null) {
+      reader.unread(next);
     }
+    return new LexicalUnit(LexicalType.NL);
+  }
 
-    private LexicalUnit getLiteral(String target) throws IOException {
-        while (true) {
-            next = reader.read();
-            String n = String.valueOf((char) next);
-            if (next == -1 || special.containsKey(n)) {
-                System.err.println("字句解析エラー: ... \"" + target + "\"");
-                System.err.println("\"で閉じられませんでした。");
-                System.exit(1);
-            }
-            target += n;
-            if (target.matches(REG_LITERAL)) {
-                target = target.substring(1, target.length() - 1);
-                return new LexicalUnit(LexicalType.LITERAL, new ValueImpl(target, ValueType.STRING));
-            }
-        }
+  private LexicalUnit getName(String target) throws IOException {
+    while (true) {
+      next = reader.read();
+      String n = String.valueOf((char) next);
+      if ((target + n).matches(REG_NAME)) {
+        target += n;
+      } else {
+        reader.unread(next);
+        break;
+      }
     }
-
-    private LexicalUnit getOperator(String target) throws IOException {
-        while (true) {
-            next = reader.read();
-            String n = String.valueOf((char) next);
-            if (operator.containsKey(target + n)) {
-                target += n;
-            } else {
-                reader.unread(next);
-                return operator.get(target);
-            }
-        }
+    LexicalUnit lu = reserved.get(target);
+    if (lu == null) {
+      return new LexicalUnit(LexicalType.NAME, new ValueImpl(target));
     }
+    return lu;
+  }
 
-    private int skipBlank() {
-        int c;
-        try {
-            do {
-                c = reader.read();
-            } while (String.valueOf((char) c).matches(REG_BLANK));
-            return c;
-        } catch (Exception e) {
-            return -1;
-        }
+  private LexicalUnit getNumber(String target) throws IOException {
+    while (true) {
+      next = reader.read();
+      String n = String.valueOf((char) next);
+      if ((target + n).matches(REG_NUMBER)) {
+        target += n;
+      } else {
+        reader.unread(next);
+        break;
+      }
     }
-
-    @Override
-    public boolean expect(LexicalType type) throws Exception {
-        // TODO 自動生成されたメソッド・スタブ
-        return false;
+    if (target.matches(REG_INTVAL)) {
+      return new LexicalUnit(LexicalType.INTVAL, new ValueImpl(target, ValueType.INTEGER));
+    } else {
+      return new LexicalUnit(LexicalType.DOUBLEVAL, new ValueImpl(target, ValueType.DOUBLE));
     }
+  }
 
-    @Override
-    public void unget(LexicalUnit token) throws Exception {
-        // TODO 自動生成されたメソッド・スタブ
+  private LexicalUnit getLiteral(String target) throws IOException {
+    while (true) {
+      next = reader.read();
+      String n = String.valueOf((char) next);
+      if (next == -1 || special.containsKey(n)) {
+        System.err.println("字句解析エラー: ... \"" + target + "\"");
+        System.err.println("\"で閉じられませんでした。");
+        System.exit(1);
+      }
+      target += n;
+      if (target.matches(REG_LITERAL)) {
+        target = target.substring(1, target.length() - 1);
+        return new LexicalUnit(LexicalType.LITERAL, new ValueImpl(target, ValueType.STRING));
+      }
     }
+  }
 
+  private LexicalUnit getOperator(String target) throws IOException {
+    while (true) {
+      next = reader.read();
+      String n = String.valueOf((char) next);
+      if (operator.containsKey(target + n)) {
+        target += n;
+      } else {
+        reader.unread(next);
+        return operator.get(target);
+      }
+    }
+  }
+
+  private int skipBlank() {
+    int c;
+    try {
+      do {
+        c = reader.read();
+      } while (String.valueOf((char) c).matches(REG_BLANK));
+      return c;
+    } catch (Exception e) {
+      return -1;
+    }
+  }
+
+  @Override
+  public boolean expect(LexicalType type) throws Exception {
+    // TODO 自動生成されたメソッド・スタブ
+    return false;
+  }
+
+  @Override
+  public void unget(LexicalUnit token) throws Exception {
+    // TODO 自動生成されたメソッド・スタブ
+  }
 }
--- src/newlang3/Value.java
+++ src/newlang3/Value.java
@@ -1,35 +1,30 @@
 package newlang3;
 
 public abstract class Value {
-    // 実装すべきコンストラクタ
-    public Value(String s) {
-    };
+  // 実装すべきコンストラクタ
+  public Value(String s) {};
 
-    public Value(int i) {
-    };
+  public Value(int i) {};
 
-    public Value(double d) {
-    };
+  public Value(double d) {};
 
-    public Value(boolean b) {
-    };
+  public Value(boolean b) {};
 
-    public Value(String s, ValueType t) {
-    };
+  public Value(String s, ValueType t) {};
 
-    public abstract String get_sValue();
+  public abstract String get_sValue();
 
-    public abstract String getSValue();
+  public abstract String getSValue();
 
-    // ストリング型で値を取り出す。必要があれば、型変換を行う。
-    public abstract int getIValue();
+  // ストリング型で値を取り出す。必要があれば、型変換を行う。
+  public abstract int getIValue();
 
-    // 整数型で値を取り出す。必要があれば、型変換を行う。
-    public abstract double getDValue();
+  // 整数型で値を取り出す。必要があれば、型変換を行う。
+  public abstract double getDValue();
 
-    // 小数点型で値を取り出す。必要があれば、型変換を行う。
-    public abstract boolean getBValue();
+  // 小数点型で値を取り出す。必要があれば、型変換を行う。
+  public abstract boolean getBValue();
 
-    // 論理型で値を取り出す。必要があれば、型変換を行う。
-    public abstract ValueType getType();
+  // 論理型で値を取り出す。必要があれば、型変換を行う。
+  public abstract ValueType getType();
 }
