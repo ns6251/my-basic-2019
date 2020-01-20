8488e379a19c913382eca9ad3aa8e8683e3eeaa8
Shota Nakagawa <ns6251@gmail.com>
2020-01-20 16:07:32 +0900

    Add CallFuncNode

--- src/newlang4/CallFuncNode.java
+++ src/newlang4/CallFuncNode.java
@@ -2,17 +2,46 @@ package newlang4;
 
 public class CallFuncNode extends Node {
 
+  private String funcName;
+  private Node exprList;
+
+  /* false: this is call_sub
+   * true:  call_func; exists return value
+   */
+  private boolean isFunc;
+
   private CallFuncNode(Environment env) {
     super(NodeType.FUNCTION_CALL, env);
   }
 
-  public static Node getHandler(Environment env) {
-    return null;
+  public static Node getHandler(Environment env) throws Exception {
+    if (env.getInput().expect(LexicalType.NAME)) {
+      LexicalUnit lu = env.getInput().peek(2);
+      if (lu.getType() == LexicalType.LP || ExprListNode.isFirst(lu)) return new CallFuncNode(env);
+    }
+    throw new Exception("Invalid");
   }
 
   @Override
   public boolean parse() throws Exception {
-    return super.parse();
+    LexicalUnit lu = env.getInput().get();
+    if (lu.getType() != LexicalType.NAME) throw new Exception("Invalid");
+    this.funcName = lu.getValue().getSValue();
+
+    if (env.getInput().expect(LexicalType.LP)) {
+      env.getInput().get();
+      this.isFunc = true;
+    }
+
+    if (!ExprListNode.isFirst(env.getInput().peek())) throw new Exception("syntax exception");
+    exprList = ExprListNode.getHandler(env);
+    exprList.parse();
+
+    if (this.isFunc)
+      if (env.getInput().get().getType() != LexicalType.RP)
+        throw new Exception("expr_list has not closed by ')'");
+
+    return true;
   }
 
   @Override
@@ -22,6 +51,7 @@ public class CallFuncNode extends Node {
 
   @Override
   public String toString() {
-    return super.toString();
+    String ret = funcName + "(\n" + exprList.toString() + "\n)";
+    return ret;
   }
 }
